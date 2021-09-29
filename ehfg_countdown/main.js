e51b1a617ab6b304b81ba333ee5fde34e3c8d1bf
rustemia98 <a123456@univie.ac.at>
2021-09-29 10:53:15 +0200

    Final Version

--- ehfg_countdown/main.js
+++ ehfg_countdown/main.js
@@ -6,66 +6,80 @@ function getParameter(parameterID) {
 
 //fetching from .json and displaying the name of the session
 fetch("./programme.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (name) {
-    document.getElementById("id").innerHTML +=
-      name[getParameter("pos")].eventname;
+  .then((response) => response.json())
+  .then((responseAsJson) => responseAsJson[getParameter("pos")])
+  .then((event) => {
+    document.getElementById("id").innerHTML += event.eventname;
   });
 
 //fetching from .json and displaying the headline of the session on the bottom of the screen in a sliding div
 fetch("./programme.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (sponsor) {
-    document.getElementById("headline").innerHTML +=
-      sponsor[getParameter("pos")].headline;
+  .then((response) => response.json())
+  .then((responseAsJson) => responseAsJson[getParameter("pos")])
+  .then((sponsor) => {
+    document.getElementById("link").target = sponsor.targetlink;
+    document.getElementById("link").href = sponsor.link;
+    document.getElementById("headline").innerHTML += sponsor.headline;
   });
 
-//initialization of the launchDate
+fetch("./programme.json")
+  .then((response) => response.json())
+  .then((responseAsJson) => responseAsJson[getParameter("pos")])
+  .then((session) => {
+    let sessionDate = session.date.split("-");
+    let sessionTime = session.start.split(":");
+    let sessionEnd = session.end.split(":");
 
-let data = null;
-let time = null;
-let launchDate = fetch("./programme.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (zeit) {
-    data = zeit[getParameter("pos")].date;
-    time = zeit[getParameter("pos")].start;
-    launchDate = new Date(`${data}, ${time}`);
+    let startDate = new Date(
+      Date.UTC(
+        sessionDate[0],
+        sessionDate[1] - 1,
+        sessionDate[2],
+        sessionTime[0] - 2, // GMT to UTC
+        sessionTime[1],
+        0
+      )
+    );
 
-    tick().then((r) => setInterval(tick, 1000));
+    let endDate = new Date(
+      Date.UTC(
+        sessionDate[0],
+        sessionDate[1] - 1,
+        sessionDate[2],
+        sessionEnd[0] - 2, // GMT to UTC
+        sessionEnd[1],
+        0
+      )
+    );
+
+    setInterval(() => tick(startDate, endDate), 1000);
   });
 
-let now = null;
-async function tick() {
-  calcTime("austria", "+2");
-  let t = launchDate - now;
+async function tick(launchDate, endDate) {
+  let difference = launchDate - new Date();
 
-  console.log(t);
-  if (t >= 300000) {
-    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+  if (difference >= 300000) {
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
 
     if (days < 10) {
       days = "0" + days;
     }
 
-    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
 
     if (hours < 10) {
       hours = "0" + hours;
     }
 
-    let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
 
     if (minutes < 10) {
       minutes = "0" + minutes;
     }
 
-    let seconds = Math.floor((t % (1000 * 60)) / 1000);
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
     if (seconds < 10) {
       seconds = "0" + seconds;
     }
@@ -79,38 +93,18 @@ async function tick() {
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
-
-    // Not sure if we event want to make that dynamic?! Prob constant is good ;)
-    // if (days === 1) {
-    //     document.getElementById("daystext").innerHTML = "day";
-    // } else {
-    //     document.getElementById("daystext").innerHTML = "days";
-    // }
-    // if (hours === 1) {
-    //   document.getElementById("hourstext").innerHTML = "hour";
-    // } else {
-    //   document.getElementById("hourstext").innerHTML = "hours";
-    // }
-    // if (minutes === 1) {
-    //   document.getElementById("minutestext").innerHTML = "minute";
-    // } else {
-    //   document.getElementById("minutestext").innerHTML = "minutes";
-    // }
-    // if (seconds === 1) {
-    //   document.getElementById("secondstext").innerHTML = "second";
-    // } else {
-    //   document.getElementById("secondstext").innerHTML = "seconds";
-    // }
   } else {
-    document.getElementById("comingsoon").innerText = "Live Now!";
-    // Visibility - Show Button / Hide Clock
-    document.getElementById("clock").style.display = "none";
-    document.getElementById("button").style.display = "inline";
+    if (endDate < new Date()) {
+      document.getElementById("comingsoon").style.fontSize = "x-large";
+      document.getElementById("comingsoon").innerText =
+        "You just missed it! A recording of this session will be available after the conference.";
+      document.getElementById("clock").style.display = "none";
+    } else {
+      document.getElementById("comingsoon").innerText = "Live Now!";
+
+      // Visibility - Show Button / Hide Clock
+      document.getElementById("clock").style.display = "none";
+      document.getElementById("button").style.display = "inline";
+    }
   }
-}
-function calcTime(city, offset) {
-  let b = new Date();
-  let utc = b.getTime() + b.getTimezoneOffset() * 60000;
-  let nd = new Date(utc + 3600000 * offset);
-  now = nd;
 }
